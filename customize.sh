a024a5cf0a1889344a1671ea3a04c40e23ca3355
purefish <58912207+purefish@users.noreply.github.com>
2019-12-20 11:24:21 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,6 +9,6 @@
 sed -i 's/192.168.100.1/192.168.100.2/g' openwrt/package/base-files/files/bin/config_generate
 cd ./package
 git clone https://github.com/Ameykyl/luci-app-koolproxyR.git
-src-git lienol https://github.com/Lienol/openwrt-package
+git clone https://github.com/Lienol/openwrt-package.git
 ./scripts/feeds update -a
 ./scripts/feeds install -a
