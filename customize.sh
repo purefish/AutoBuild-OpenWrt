0e49ea7c61648bb5134a63930214455fc373f98e
purefish <58912207+purefish@users.noreply.github.com>
2019-12-20 10:59:27 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,7 +7,7 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.100.1/192.168.100.2/g' openwrt/package/base-files/files/bin/config_generate
-cd package
+cd ./package
 git clone https://github.com/Ameykyl/luci-app-koolproxyR.git
 src-git lienol https://github.com/Lienol/openwrt-package
 ./scripts/feeds update -a
